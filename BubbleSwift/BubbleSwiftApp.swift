ff6e6b15f54a77c8fe7811e016388c029abaae81
Viet Tung Le <viettung.le.2906@gmail.com>
2023-07-26 20:07:19 +0700

    Initial Commit

---
+++ BubbleSwift/BubbleSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleSwiftApp.swift
+//  BubbleSwift
+//
+//  Created by Le Viet Tung on 26/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BubbleSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
